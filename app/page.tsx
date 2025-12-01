09eeb286c7f012225db250225e66f628a4bbeb1c
B-106 <164723337+B-106@users.noreply.github.com>
2025-12-01 20:44:31 +0900

    db connect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import { supabase } from "@/lib/supabase";
 
-export default function Home() {
+export default async function Home() {
+  const { data, error } = await supabase.from("var1").select("*");
   return (
     <main className="p-6 text-2xl">
       My Own Flashcard 홈 화면입니다.
+      <div>
+        <h1>Cards</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
     </main>
   );
 }
